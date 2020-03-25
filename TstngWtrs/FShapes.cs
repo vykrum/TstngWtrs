295bd4838b7b917e5d5b72118e314abe8758dd2d
Vikram Subbaiah <vykrum@hotmail.com>
2020-03-25 18:54:14 +0530

    Trial multiple clusters in FShapes.cs

--- TstngWtrs/FShapes.cs
+++ TstngWtrs/FShapes.cs
@@ -8,10 +8,12 @@ namespace TstngWtrsF
     {
         internal FShapes() { }
         
-        public static FSharpList<FSharpList<double>> Clstr(int n = 1, double x = 0.0, double y = 0.0)
+        public static FSharpList<FSharpList<FSharpList<double>>> Clstr(int c = 1, int n = 1, double x = 0.0, double y = 0.0)
         {
-            var tup = HexShapes.cls (x, y, n, HexShapes.adj( 0.0, 1.074 ) );
-            return HexShapes.tupXY(tup);
+            var tup = HexShapes.cls (x, y, n, HexShapes.adj( 0.0, 1074 ) );
+            var xm = HexShapes.mcl(c, tup, HexShapes.adj(0.0, 1074));
+
+            return HexShapes.tupLst(xm);
             //var prm = HexShapes.prm(tup);
             //return HexShapes.tupXY( prm );            
         }
